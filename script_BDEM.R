86b05bfdf65c2e9889fd9a1b6bacb9eea79d40fe
raquelam09 <moreiraraquel@id.uff.br>
2026-09-06 23:45:42 -0300

    script BDEM - SIM - tarefas 1 a 4

--- script_BDEM.R
+++ script_BDEM.R
@@ -66,6 +66,16 @@ nrow(dados_sim_2)
 # Atenção: a unidade de medida de IDADE no DICIONÀRIO do SIM está errada
 # O propósito das avaliações acima é verificar se as categorias estão de acordo com o dicionário do SIM ou se aparecem categorias estranhas
 
+# Verificar a frequência das variáveis categóricas
+table(dados_sim_2$TIPOBITO, useNA = "ifany")
+table(dados_sim_2$SEXO, useNA = "ifany")
+table(dados_sim_2$RACACOR, useNA = "ifany")
+table(dados_sim_2$ESC2010, useNA = "ifany")
+table(dados_sim_2$TPMORTEOCO, useNA = "ifany")
+table(dados_sim_2$CAUSABAS, useNA = "ifany")
+
+# Avaliar os valores da variável IDADE
+table(dados_sim_2$IDADE, useNA = "ifany")
 
 # Ao terminar a Tarefa 4 commit com a mensagem "script BDEM - SIM - tarefas 1 a 4" e envie para o repositório Projeto_BDEM_2016
 
