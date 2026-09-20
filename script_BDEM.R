dbe349c926f56efa188250d74156ae1d5547c613
raquelam09 <moreiraraquel@id.uff.br>
2026-09-20 11:45:56 -0300

    script BDEM - SINASC - tarefas 1 a 2

--- script_BDEM.R
+++ script_BDEM.R
@@ -661,6 +661,16 @@ names(dados_sinasc)
 # Nomes das respectivas variáveis: CODMUNNASC, LOCNASC, IDADEMAE, ESTCIVMAE, CODMUNRES, GESTACAO, GRAVIDEZ, PARTO, 
 # SEXO, APGAR5, RACACOR, PESO, IDANOMAL, ESCMAE2010, RACACORMAE, SEMAGESTAC, TPAPRESENT, TPROBSON, PARIDADE, KOTELCHUCK, CONTADOR
 
+# Criar o novo banco de dados selecionando as colunas pelos números indicados
+dados_sinasc_1 <- dados_sinasc[, c(3, 4, 5, 6, 11, 12, 13, 14, 18, 20, 21, 22, 23, 34, 37, 43, 47, 58, 59, 60, 61)]
+
+# Verificar se os nomes das colunas estão de acordo com o roteiro
+names(dados_sinasc_1)
+
+# Verificar a dimensão do novo banco
+dim(dados_sinasc_1)
+
+
 
 # Ao terminar a Tarefa 2 commit com a mensagem "script BDEM - SINASC - tarefas 1 a 2" e envie para o repositório Projeto_BDEM_2016
 
